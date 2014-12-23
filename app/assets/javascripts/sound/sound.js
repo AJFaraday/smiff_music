ca6f6f12340da83b2110a9f39bf346e31c4c7278
ajfaraday <andrewfaraday@hotmail.co.uk>
2014-12-23 23:03:04 +0000

    Player works, has buttons, is updated while playing

--- app/assets/javascripts/sound/sound.js
+++ app/assets/javascripts/sound/sound.js
@@ -3,14 +3,17 @@ var Sound = {
   context:null,
   samples:{},
   patterns:{},
-  sixteenth_time: 150,
-  step: 0,
+  sixteenth_time:150,
+  step:0,
+  lowest_common_multiple:0,
+  player_active:false,
+
 
   init:function (attributes) {
     // set up context and gain chain
     this.get_context();
     this.master_gain = this.context.createGainNode();
-    this.master_gain.value = 0.5;
+    this.master_gain.value = 0.2;
     this.master_gain.connect(this.context.destination);
 
     // load samples
@@ -19,12 +22,29 @@ var Sound = {
       Sound.samples[sample_name].load_sample();
     });
     this.set_patterns(attributes['patterns']);
+    this.init_player_controls();
+  },
+
+  init_player_controls:function () {
+    $('#play_button').on('click', function () {
+      Sound.play()
+    });
+    $('#stop_button').on('click', function () {
+      Sound.stop()
+    });
   },
 
   set_patterns:function (patts) {
     $.each(patts, function (key, attrs) {
       Sound.patterns[key] = new Pattern(attrs)
     });
+
+    var step_counts = []
+    for (key in Sound.patterns) {
+      step_counts = step_counts.concat(Sound.patterns[key].steps.length);
+    }
+    ;
+    Sound.lowest_common_multiple = Sound.LCM(step_counts);
   },
 
   reset_patterns:function () {
@@ -60,16 +80,50 @@ var Sound = {
     $.each(this.patterns, function (name, pattern) {
       pattern.play_step(step)
     });
-    this.step += 1
+    this.step += 1;
+    this.reset_step();
   },
 
-  play: function() {
-    setInterval(
-      function() {Sound.play_step(Sound.step) },
-      this.sixteenth_time
-    )
+  reset_step:function () {
+    if ((Sound.step % Sound.lowest_common_multiple) == 0) {
+      Sound.step = 0;
+      Sound.reset_patterns();
+    }
+  },
+
+  play:function () {
+    if (!Sound.player_active) {
+      Sound.step = 0;
+      Sound.player = setInterval(
+        function () {
+          Sound.play_step(Sound.step)
+        },
+        this.sixteenth_time
+      )
+      Sound.player_active = true;
+    }
+  },
+
+  stop:function () {
+    if (Sound.player_active) {
+      clearInterval(Sound.player);
+      Sound.player_active = false;
+    }
+  },
+
+
+  // lowest common multiple, pinched from
+  // http://rosettacode.org/wiki/Least_common_multiple#JavaScript
+  LCM:function (A) {
+    var n = A.length, a = Math.abs(A[0]);
+    for (var i = 1; i < n; i++) {
+      var b = Math.abs(A[i]), c = a;
+      while (a && b) {
+        a > b ? a %= b : b %= a;
+      }
+      a = Math.abs(c * A[i]) / (a + b);
+    }
+    return a;
   }
-
-
 
 }
--- db/seeds.rb
+++ db/seeds.rb
@@ -15,15 +15,9 @@ Pattern.create(
   active: true
 )
 
-sample_names = %w(kick snare)
+
+
+sample_names = Pattern.all.collect{|x|x.instrument_name}
 sample_names.each do |sample|
   Sample.create(name: sample)
 end
-
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
