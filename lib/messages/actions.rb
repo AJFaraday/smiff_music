67b6816de02dd37aba89042395fde40d56a5b386
ajfaraday <andrewfaraday@hotmail.co.uk>
2014-12-26 11:46:54 +0000

    add steps actions now all work smoothly

--- lib/messages/actions.rb
+++ lib/messages/actions.rb
@@ -1,8 +1,11 @@
 class Messages::Actions
 
-  AVAILABLE_ACTIONS = ['show_patterns']
+  require 'active_support'
+
+  AVAILABLE_ACTIONS = %w{ show_patterns add_steps}
 
   extend Messages::Actions::Show
+  extend Messages::Actions::AddSteps
 
   def self.run(action, arguments)
     if Messages::Actions::AVAILABLE_ACTIONS.include?(action)
@@ -11,11 +14,23 @@ class Messages::Actions
       return {
         response: 'failure',
         display: I18n.t(
-        'messages.errors.not_implemented',
-        action: action
-      )
+          'messages.errors.not_implemented',
+          action: action
+        )
       }
     end
+  end
+
+  def self.pattern_not_found(pattern_names)
+    {
+      response: 'failure',
+      display: I18n.t(
+        'actions.show_patterns.errors.no_pattern',
+        names: pattern_names.to_sentence(
+          last_word_connector: ' and '
+        )
+      )
+    }
   end
 
   def self.munge_list(list)
---
+++ lib/messages/actions/add_steps.rb
@@ -0,0 +1,55 @@
+module Messages::Actions::AddSteps
+
+
+
+  attr_accessor :pattern
+  attr_accessor :steps
+
+  def add_steps(args)
+    self.pattern = Pattern.find_by_name(args['pattern_name'])
+    return pattern_not_found([args['pattern_name']]) unless self.pattern
+
+    if args.keys.include?('step')
+      set_steps([args['step']])
+    elsif args.keys.include?('start_step') and args.keys.include?('end_step')
+      if args.keys.include?('block_size')
+        puts 'skipping'
+        set_block_skipping(args)
+      else
+        puts "not skipping #{args.inspect}"
+        set_block(args)
+      end
+    end
+  end
+
+  # this sets the array, steps, indexes to true
+  def set_steps(steps)
+    modified_steps = steps.collect{|x| x.to_i - 1}
+    self.pattern.pattern_indexes += modified_steps
+    self.pattern.save!
+    {
+      response: 'success',
+      display: I18n.t(
+        'actions.add_steps.success',
+        name: self.pattern.name,
+        steps: steps.to_sentence(
+          last_word_connector: ' and '
+        )
+      )
+    }
+  end
+
+  def set_block(args)
+    steps = (args['start_step']..args['end_step']).to_a
+    set_steps(steps)
+  end
+
+  def set_block_skipping(args)
+    edges = [args['start_step'].to_i, args['end_step'].to_i]
+    steps = (edges.min..edges.max).to_a
+    steps.reject!{|x| (x - edges.min + 1) % (args['block_size'].to_i + 1) != 1}
+    set_steps(steps)
+  end
+
+
+end
