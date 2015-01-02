1d995134b0e967456f938c22040a85ea9fcab87f
ajfaraday <andrewfaraday@hotmail.co.uk>
2015-01-02 14:21:58 +0000

    correctly formatted clear all action

--- lib/messages/actions/clear_all_drums.rb
+++ lib/messages/actions/clear_all_drums.rb
@@ -3,7 +3,7 @@ module Messages::Actions::ClearAllDrums
   # note: this may be 'antisocial' when there is more than one user
   def clear_all_drums(args)
     # todo when there are non-drum patterns, distinguish between drums and melodic.
-    Pattern.update_all(:bits, 0)
+    Pattern.update_all(bits: 0)
 
     return {
       response: 'success',
