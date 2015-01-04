b31c9ad9495bfbe336434c72443a5d02494a8418
ajfaraday <andrewfaraday@hotmail.co.uk>
2015-01-04 20:30:20 +0000

    mute and unmute all now updates the pattern store accordingly

--- lib/messages/actions/mute_unmute_all.rb
+++ lib/messages/actions/mute_unmute_all.rb
@@ -3,6 +3,8 @@ module Messages::Actions::MuteUnmuteAll
   def mute_unmute_all(args)
     mode = args['mode'][0]
     Pattern.update_all(muted: mode == 'mute')
+    PatternStore.hash.each{|k,value| value[:muted] = mode == 'mute' if value.is_a?(Hash)}
+    PatternStore.version += 1
     return {
       response: 'success',
       display: I18n.t(
