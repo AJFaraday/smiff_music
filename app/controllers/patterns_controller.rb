d0eb2913a8c1326b6d938f1c3cb6b276a06b6cd3
ajfaraday <andrewfaraday@hotmail.co.uk>
2015-01-18 19:08:36 +0000

    added midi note display method, and implemented it

--- app/controllers/patterns_controller.rb
+++ app/controllers/patterns_controller.rb
@@ -1,5 +1,7 @@
 class PatternsController < ApplicationController
-  
+
+  helper SynthsHelper
+
   def index
     respond_to do |format|
       format.html do
---
+++ app/helpers/synths_helper.rb
@@ -0,0 +1,11 @@
+module SynthsHelper
+
+  NOTE_NAMES = %w(A A# B C C# D D# E F F# G G#)
+
+  def display_midi_note(midi)
+    note_name = NOTE_NAMES[(midi - 21) % 12]
+    octave = (midi - 12) / 12
+    "#{note_name} #{octave}"
+  end
+
+end
