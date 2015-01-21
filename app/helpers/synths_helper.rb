8fd88a0539866e448099bd6590073fca47c10067
ajfaraday <andrewfaraday@hotmail.co.uk>
2015-01-21 21:07:09 +0000

    moved class decision out to a helper

--- app/helpers/synths_helper.rb
+++ app/helpers/synths_helper.rb
@@ -3,6 +3,23 @@ module SynthsHelper
   NOTE_NAMES = %w(A A# B C C# D D# E F F# G G#)
 
 
+  def step_class(synth, step,note)
+    kls = (step % 4 == 0) ? 'step marker ' : 'step '
+    if synth.pitch_at_step(step) == note
+      if synth.patterns.note_on.pattern_indexes.include?(step)
+        kls << 'note_start'
+      elsif synth.active_at_step(step)
+        kls << 'note_continues'
+      elsif synth.patterns.note_off.pattern_indexes.include?(step)
+        kls << 'note_end'
+      else
+        kls << 'inactive'
+      end
+    else
+      kls << 'inactive'
+    end
+    kls
+  end
 
   def display_midi_note(midi)
     note_name = NOTE_NAMES[(midi - 21) % 12]
