e4efe7ce8813394a86a43611a273e1cc228d86b9
ajfaraday <andrewfaraday@hotmail.co.uk>
2015-01-24 14:14:32 +0000

    avoided duplicate drum patterns

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,21 @@
-
 # generate drum patterns (simple 32-step form)
 %w(kick snare hihat crash tom1 tom2 tom3).each do |drum|
-  Pattern.create(
-    name: drum,
-    step_size: 16,
-    step_count: 32,
-    instrument_name: drum,
-    active: true,
-    purpose: 'event'
-  )
+  unless Pattern.find_by_name(drum)
+    Pattern.create(
+      name: drum,
+      step_size: 16,
+      step_count: 32,
+      instrument_name: drum,
+      active: true,
+      purpose: 'event'
+    )
+  end
 end
 
-sample_names = Pattern.where(purpose: 'event').collect{|x|x.instrument_name}
+sample_names = Pattern.where(purpose: 'event').collect { |x| x.instrument_name }
 sample_names.each do |sample|
   Sample.create(name: sample)
 end
-
 
 
 Messages::FormatBuilder.new.build_messages
