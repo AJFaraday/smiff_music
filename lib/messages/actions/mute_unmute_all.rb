3992601e2bb2d47608685bfb76d84ed48b555fce
ajfaraday <andrewfaraday@hotmail.co.uk>
2015-01-28 21:12:51 +0000

    mute/unmute all now works perfectly

--- lib/messages/actions/mute_unmute_all.rb
+++ lib/messages/actions/mute_unmute_all.rb
@@ -1,17 +1,28 @@
 module Messages::Actions::MuteUnmuteAll
 
   def mute_unmute_all(args)
-    mode = args['mode'][0]
-    Pattern.update_all(muted: mode == 'mute')
-    PatternStore.hash['patterns'].each{|k,value| value[:muted] = mode == 'mute' if value.is_a?(Hash)}
+    mode = args['mode']
+    group = args['group']
+    if ['', ' drums'].include?(group)
+      puts 'muting drums'
+      Pattern.update_all(muted: mode == 'mute')
+      PatternStore.hash['patterns'].each { |k, value| value[:muted] = mode == 'mute' if value.is_a?(Hash) }
+    end
+    if ['', ' synths'].include?(group)
+      puts 'muting synths'
+      Synth.update_all(muted: mode == 'mute')
+      PatternStore.hash['synths'].each { |k, value| value[:muted] = mode == 'mute' if value.is_a?(Hash) }
+    end
+
     PatternStore.version += 1
     return {
       response: 'success',
       display: I18n.t(
         "actions.mute_unmute_all.success",
-        action: I18n.t("actions.mute_unmute_all.#{mode}")
+        action: I18n.t("actions.mute_unmute_all.#{mode}"),
+        group: group.singularize
       )
     }
-  end 
- 
+  end
+
 end
