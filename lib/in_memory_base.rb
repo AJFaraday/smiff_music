e8ca2bfff22a7b26e206fd9a96f33fe0c5f47a72
ajfaraday <andrewfaraday@hotmail.co.uk>
2015-05-20 21:02:46 +0100

    Wrapped common features of in-memory classes to a common class

---
+++ lib/in_memory_base.rb
@@ -0,0 +1,27 @@
+class InMemoryBase < OpenStruct
+
+  cattr_accessor :all
+
+  def initialize(attrs={})
+    super(attrs)
+    self.class.all ||= []
+    self.class.all << self
+  end
+
+  def self.build
+    source_records_path = File.join(Rails.root, 'config','seeds',"#{self.to_s.underscore.pluralize}.yml")
+    if File.exist?(source_records_path)
+      seeds = YAML.load_file(source_records_path)
+      seeds.each do |label,attributes|
+        self.new(attributes)
+      end
+      puts "Initialised #{self.all.count} #{self} objects"
+    end
+  rescue => er
+    puts er.class
+    puts er.message
+    puts er.backtrace.join("\n")
+  end
+
+
+end
