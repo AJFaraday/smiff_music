2137dd3698764232e583fcdeb8649698c1132757
ajfaraday <andrewfaraday@hotmail.co.uk>
2015-06-03 22:54:10 +0100

    Initial help guesser stuff

---
+++ lib/context_help.rb
@@ -0,0 +1,57 @@
+module ContextHelp
+
+  extend ContextHelp::Guesser
+
+  cattr_accessor :types
+
+  def ContextHelp.types
+    @@types ||= YAML.load_file(File.join(Rails.root, 'lib', 'context_help', 'types.yml'))
+  end
+
+  def ContextHelp.type_names
+    ContextHelp.types.keys
+  end
+
+  def ContextHelp.for(type, object_name=nil)
+    if ContextHelp.type_names.include?(type)
+      return ContextHelp.for_object(type, object_name) if object_name
+      ContextHelp.for_type(type)
+    else
+      raise "This type of context sensitive help is unknown: #{type}"
+    end
+  end
+
+  def ContextHelp.for_object(type, object_name)
+    source_file = File.join(Rails.root, 'docs', 'context_help', "#{type}.md")
+    format = ContextHelp.types[type]
+    object = format['class'].constantize.find_by_name(object_name)
+    subs = {}
+    format['substitutions'].each do |name, method|
+      subs[name] = object.send(method)
+    end
+    ContextHelp.render_file(source_file, subs)
+  end
+
+  def ContextHelp.for_type(type)
+    type = type.pluralize
+    source_file = File.join(Rails.root, 'docs', 'context_help', "#{type}.md")
+    ContextHelp.render_file(source_file)
+  end
+
+  def ContextHelp.render_file(path, substitutions={})
+    source = File.read(path)
+    ContextHelp.render(source, substitutions)
+  end
+
+  def ContextHelp.render(message, substitutions={})
+    substitutions.each do |key, value|
+      message.gsub!("{#{key}}", value.to_s)
+    end
+    message
+  end
+
+  def ContextHelp.help_unavailable
+    I18n.t('context_help.unavailable')
+  end
+
+end
---
+++ lib/context_help/guesser.rb
@@ -0,0 +1,24 @@
+module ContextHelp::Guesser
+
+  cattr_accessor :record_names
+
+  def guess(text)
+    text.downcase!
+    self.types.each do |name, type|
+      # check if any of the named records are mentioned
+      if type['class']
+        model = type['class'].constantize
+        model.all.each do |record|
+          if text.include?(record.name)
+            return self.for(name, record.name)
+          end
+        end
+      end
+      # look to see if any synonyms are mentioned
+      return self.for(name) if type['synonyms'].any? { |syn| text.include?(syn) }
+    end
+    # if nothing can be found, apologise
+    self.help_unavailable
+  end
+
+end
