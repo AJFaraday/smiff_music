6e5dae12ac75a97f5633d5398b0b41b8fcd38810
ajfaraday <andrewfaraday@hotmail.co.uk>
2016-01-28 19:27:28 +0000

    remoed a pile of stuff from the synth model

--- app/models/synth.rb
+++ app/models/synth.rb
@@ -245,44 +245,6 @@ TEXT
     params
   end
 
-  attr_accessor :chart_data
-
-  def chart_data
-    return @chart_data if @chart_data
-    @chart_data = []
-    envelope_times = [
-      (attack_time * 1000).to_i,
-      (decay_time * 1000).to_i,
-      (release_time * 1000).to_i
-    ]
-    gcd = envelope_times.reduce(:gcd)
-
-    @chart_data << 0
-    (((attack_time * 1000) / gcd) - 1).to_i.times { @chart_data << nil }
-    @chart_data << 1
-    (((decay_time * 1000) / gcd) - 1).to_i.times { @chart_data << nil }
-    @chart_data << sustain_level
-    (((attack_time * 1000) + (decay_time * 1000)) / gcd).to_i.times { @chart_data << nil }
-    @chart_data << sustain_level
-    (((release_time * 1000) / gcd) - 1).to_i.times { @chart_data << nil }
-    @chart_data << 0
-    @chart_data
-  end
-
-  # This uses chart_data above to generate full params for the chart plugin
-  def chart_data_full
-    {
-      labels: Array.new(chart_data.length, ''),
-      datasets: [
-        {
-          fillColor: "rgba(220,220,220,0.2)",
-          strokeColor: "rgba(220,220,220,1)",
-          data: chart_data
-        }
-      ]
-    }
-  end
-
   def add_note(pitch, start_step, length)
     end_step = start_step + (length - 1)
     clear_range(start_step, end_step)
